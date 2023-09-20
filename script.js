a93b1eac6886c111f9fbc333bdc73e36e32e8e99
aziz241 <ozkayaazizcan241@gmail.com>
2023-09-20 13:48:01 +0300

    simple to-do app

---
+++ script.js
@@ -0,0 +1,83 @@
+"use strict";
+var div = document.getElementById('gorevdiv');
+
+var text = document.getElementById('gorevtext');
+var buton = document.getElementById('gorevbuton');
+
+var silbuton = document.getElementById('temizlebuton');
+
+var gorevler = new Array();
+
+
+const displaytasks = function (event) {
+    let ul = document.getElementById('gorevul');
+    ul.innerHTML = "";
+
+
+    for (let gorev of gorevler) {
+
+        var li = `
+            <li id="${gorev.gorevid}"> ${gorev.gorevadi} <a href="#" onclick="removeone(${gorev.gorevid})" class="btn btn-danger">Sil</a></li>
+            
+            `;
+
+        ul.insertAdjacentHTML('beforeend', li);
+
+    }
+
+
+
+
+}
+
+const add = function (gorevAdi) {
+
+    gorevler.push({ 'gorevid': gorevler.length + 1, 'gorevadi': gorevAdi })
+
+
+
+}
+
+const remove = function () {
+
+
+
+    gorevler = [];
+
+
+}
+const removeone = function (id) {
+    console.log(id)
+    var removeid;
+    for (let index in gorevler) {
+        if (gorevler[index].gorevid == id) {
+            removeid = index;
+        }
+    }
+    console.log(removeid);
+    gorevler.splice(removeid, 1);
+    displaytasks();
+
+}
+buton.addEventListener("click", function () {
+
+    if (text.value == "") {
+        alert("Lütfen görev giriniz!")
+
+    }
+    else {
+        add(text.value);
+        displaytasks();
+        text.value = '';
+    }
+
+
+})
+
+silbuton.addEventListener("click", function () {
+
+    remove();
+    displaytasks();
+})
+
+
